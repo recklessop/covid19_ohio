8ed8bb688f6f319156d5eae76ee2ade90429fb0c
Justin Paul <justin@jpaul.me>
2020-11-22 08:08:39 -0500

    2020-11-22

--- covid-csv-update.sh
+++ covid-csv-update.sh
@@ -2,6 +2,7 @@
 cd /home/justin/covid19_ohio
 curl -o COVIDSummaryData.csv https://coronavirus.ohio.gov/static/COVIDSummaryData.csv
 curl -o COVIDSummaryDataZIP.csv https://coronavirus.ohio.gov/static/dashboards/COVIDSummaryDataZIP.csv
+curl -o school_reporting.csv https://coronavirus.ohio.gov/static/dashboards/school_reporting.csv
 git add *
 git commit -m "$(date '+%Y-%m-%d')"
 git push
